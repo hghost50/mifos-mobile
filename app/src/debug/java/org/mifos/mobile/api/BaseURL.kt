e93bc61d34f90cc390d01b76b76a76695e31dda5
hghost50 <119769197+hghost50@users.noreply.github.com>
2023-04-28 06:42:46 +0000

    Update BaseURL.kt

--- app/src/debug/java/org/mifos/mobile/api/BaseURL.kt
+++ app/src/debug/java/org/mifos/mobile/api/BaseURL.kt
@@ -16,7 +16,7 @@ class BaseURL {
     }
 
     companion object {
-        const val API_ENDPOINT = "192.168.8.100"
+        const val API_ENDPOINT = "bank.ebedou.com"
         const val API_PATH = "/fineract-provider/api/v1/"
         const val PROTOCOL_HTTPS = "https://"
     }
