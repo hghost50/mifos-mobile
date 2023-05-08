cef40a8b8df6966a531195c1803d05ac11d8a1eb
hghost50 <119769197+hghost50@users.noreply.github.com>
2023-05-08 23:56:11 +0000

    Update BaseURL.kt

--- app/src/release/java/org/mifos/mobile/api/BaseURL.kt
+++ app/src/release/java/org/mifos/mobile/api/BaseURL.kt
@@ -18,8 +18,8 @@ class BaseURL {
     }
 
     companion object {
-        const val API_ENDPOINT = "192.168.8.100"
-        const val API_PATH = "/fineract-provider/api/v1/self/"
+        const val API_ENDPOINT = "192.168.77.177"
+        const val API_PATH = "/fineract-provider/api/v1/"
         const val PROTOCOL_HTTPS = "https://"
     }
 }
